cf7383496fce1fc3aebca19ea951d1c1ae17594e
jildertviet <jildertviet@gmail.com>
2024-05-13 21:59:26 +0200

    Net fixture: ||||||||||||, adding Ethernet support

---
+++ examples/llllllllllll/.pio/build/esp32dev/lib0e6/FastLED/platforms.cpp.d
@@ -0,0 +1,2 @@
+.pio/build/esp32dev/lib0e6/FastLED/platforms.cpp.o: \
+ .pio/libdeps/esp32dev/FastLED/src/platforms.cpp
---
+++ examples/llllllllllll/src/main.cpp
@@ -0,0 +1,13 @@
+// char version[2] = {1, 1};
+#include "JFixLib.h"
+
+Jllll jllll;
+
+void setup() {
+  JllllSettings settings;
+  settings.networkName = "JV_";
+  settings.pins = new uint8_t[1]{22}; // Test on pin3
+  jllll.setup(settings);
+}
+
+void loop() { jllll.update(); }
