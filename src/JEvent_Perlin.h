1492d282a67742376e28052bfa5e9db6ae05228d
jildertviet <jildertviet@gmail.com>
2025-04-10 21:43:49 +0200

    Perlin acts to h and y

--- src/JEvent_Perlin.h
+++ src/JEvent_Perlin.h
@@ -25,16 +25,29 @@ public:
   void draw(floatColor **leds, int numLedsPerString, char numStrings,
             int horizontalPixelDistance) override {
     if (leds && brightness) {
+      float y =
+          loc[1] * viewport[1] - (viewportOffset[1] * viewport[1]); // In pixels
+      float h = size[1] * viewport[1];                              // in Pixels
+      float yEnd = y + h;
+
       unsigned long t = millis() - syncTime;
       for (float j = 0; j < numStrings; j++) {
-        for (float i = 0; i < numLedsPerString; i++) {
+        if (yEnd < 0 || y > viewport[1])
+          continue;
+        if (y < 0) // Clip in range
+          y = 0;
+        if (yEnd > numLedsPerString)
+          yEnd = numLedsPerString;
+
+        for (float i = y; i < yEnd; i++) {
           float val =
               ofNoise(i * noiseScale,
                       ((j * horizontalPixelDistance) + horizontalPixelOffset) *
                           noiseScale,
                       t * noiseTimeScale);
           // val = pow(
-          // val, 2.0); // Already @ writeRGB. Edit; do it anyway, looks better
+          // val, 2.0); // Already @ writeRGB. Edit; do it anyway, looks
+          // better
           val = val * val;
           // if (val < 0.005) // Do this with a background
           // val = 0.005;
