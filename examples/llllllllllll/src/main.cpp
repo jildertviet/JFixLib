879d2310a0d54228b0f524d2cdf381d88f313567
jildertviet <jildertviet@gmail.com>
2025-08-23 17:33:16 +0200

    llllllll example to espidf, arduino

--- examples/llllllllllll/src/main.cpp
+++ examples/llllllllllll/src/main.cpp
@@ -8,7 +8,13 @@ void setup() {
   settings.networkName = "JV_";
   settings.pins = new uint8_t[1]{22}; // Test on pin3
   jllll.setup(settings);
-  Serial.println(xPortGetCoreID());
 }
 
 void loop() { jllll.update(); }
+
+extern "C" void app_main(void) {
+  setup();
+  while (1) {
+    loop();
+  }
+}
